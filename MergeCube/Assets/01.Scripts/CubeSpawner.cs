5ded72ec8980142c299ef6fb71a3c618b4f1863a
SungJieundev <sungjieun.dev@gmail.com>
2022-12-26 20:29:50 +0900

    CubeSpawner

--- MergeCube/Assets/01.Scripts/CubeSpawner.cs
+++ MergeCube/Assets/01.Scripts/CubeSpawner.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour
 {
-    
+    // Object Pooling
+
+    public static CubeSpawner Instance = null;
+    Queue<Cube> cubesQueue = new Queue<Cube>();
+ 
+    [SerializeField] private int cubesQueueCapacity = 20;
+    [SerializeField] private bool autoQueueGrow = true;
+
+    [SerializeField] private GameObject cubePrefab;
+    [SerializeField] private Color[] cubeColors;
+
+    public int maxCubeNumber;
+    private int maxPower = 12; // 2^12
+
+    private Vector3 defaultSpawnPos;
+
+    private void Awake() {
+        
+        if(Instance == null) Instance = this;
+        else Destroy(gameObject);
+
+        defaultSpawnPos = transform.position;
+        maxCubeNumber = (int)Mathf.Pow(2, maxPower);
+    }
+
+    private void InitCubeQueue(){
+
+        for(int i = 0; i < cubesQueueCapacity; i++){
+
+            //큐브 생성해서 Queue에 Enqueue
+            
+        }
+    }
 }
