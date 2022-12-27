d89ecba8b1250adf597e1e37fbdfa7761952d0bf
SungJieundev <sungjieun.dev@gmail.com>
2022-12-27 18:30:06 +0900

    ObjectPooling

--- MergeCube/Assets/01.Scripts/CubeSpawner.cs
+++ MergeCube/Assets/01.Scripts/CubeSpawner.cs
@@ -34,7 +34,51 @@ public class CubeSpawner : MonoBehaviour
         for(int i = 0; i < cubesQueueCapacity; i++){
 
             //큐브 생성해서 Queue에 Enqueue
-            
+            AddCubeToQueue();
         }
+    }
+
+    private void AddCubeToQueue(){
+
+        Cube cube = Instantiate(cubePrefab, defaultSpawnPos, Quaternion.identity, transform).GetComponent<Cube>();
+
+        cube.gameObject.SetActive(false);
+        cubesQueue.Enqueue(cube);
+    }
+
+    public int GenerateRandomNumber(){
+
+        // 2^1 ~ 2^6
+        return (int)Mathf.Pow(2, Random.Range(1,6));
+    }
+
+    private Color GetColor(int number){
+
+        return cubeColors[(int)(Mathf.Log(number) / Mathf.Log(2)) -1];
+    }
+
+    public Cube Spawn(int number, Vector3 pos){
+
+        if(cubesQueue.Count == 0){
+
+            if(autoQueueGrow){
+
+                cubesQueueCapacity++;
+                AddCubeToQueue();
+            }
+            else{
+
+                Debug.Log("Queue is Empty");
+                return null;
+            }
+        }
+
+        Cube cube = cubesQueue.Dequeue();
+        cube.transform.position = pos;
+        cube.SetNumber(number);
+        cube.SetColor(GetColor(number));
+        cube.gameObject.SetActive(true);
+
+        return cube;
     }
 }
